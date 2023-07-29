82e5121ae3f17b0612a09332063dffdc07c80e65
justbenuk <justbenuk@gmail.com>
2023-07-29 20:42:11 +0100

    updated cpm breaking change

--- nvim/lua/bena/plugins/lspconfig.lua
+++ nvim/lua/bena/plugins/lspconfig.lua
@@ -2,7 +2,9 @@
 require('mason').setup()
 require('mason-lspconfig').setup({ automatic_installation = true })
 
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+-- local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 -- PHP
 require('lspconfig').intelephense.setup({capabilities = capabilities })
