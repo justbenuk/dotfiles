b26d335e18b65b49a8bb1eae2530adc683d73569
justbenuk <justbenuk@gmail.com>
2023-09-04 19:36:23 +0100

    updated rofi config

--- qtile/config.py
+++ qtile/config.py
@@ -70,7 +70,7 @@ keys = [
     Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
 
     # Launch Applications
-    Key([mod], "r", lazy.spawn('rofi -show'), desc="Spawn a command using a prompt widget"),
+    Key([mod], "r", lazy.spawn('rofi -show drun'), desc="Spawn a command using a prompt widget"),
     Key([mod], "b", lazy.spawn('google-chrome-stable'), desc="Browser"),
     Key([mod], "o", lazy.spawn('libreoffice'), desc="Open Office"),
 ]
