95489595fcf688106e0a8774f5bf5c86462949be
justbenuk <justbenuk@gmail.com>
2025-03-03 23:23:58 +0000

    updated for better typescript support

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.breakindent = true
+vim.opt.shiftwidth = 2
